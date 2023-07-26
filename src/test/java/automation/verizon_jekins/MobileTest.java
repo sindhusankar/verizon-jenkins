5f3b7c6430a2615b2ee0a263ddf11962e4958f31
sindhu <sindhusankar0205@gmail.com>
2023-07-26 14:13:53 +0530

    new commit

---
+++ src/test/java/automation/verizon_jekins/MobileTest.java
@@ -0,0 +1,24 @@
+package automation.verizon_jekins;
+
+import org.testng.annotations.Test;
+
+
+public class MobileTest 
+{
+   
+    @Test
+    
+    void login()
+    {
+    	System.out.println("Login");
+    }
+    
+    @Test
+   
+    void Logut()
+    {
+    	System.out.println("Logout");
+    
+    }
+    
+}
